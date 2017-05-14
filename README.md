1d2c634a1b5359b1aa5b052ad5df335c80c72c7c
Josh Trommel <joshtrommel@gmail.com>
2017-05-14 00:37:19 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+[songlink](https://songlink.io/) scraper
+=================
+
+a simple songlink scraper, since it didn't seem like they have a public API.
+
+don't sue me pls
+
+usage
+-----
+
+clone, `yarn install`, `node index`. `localhost:8030/?url=URL`
+
+```
+http://localhost:8030/?url=https://open.spotify.com/track/4fbvXwMTXPWaFyaMWUm9CR
+http://localhost:8030/?url=https://itun.es/ca/1uxdR?i=722302464
+```
+
+license
+-------
+
+mit
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+    , cheerio = require('cheerio')
+    , request = require('request');
+
+var app = express();
+var url = 'https://songlink.io/';
+
+app.get('/', (req, res, next) => {
+    if (!req.query || !req.query.url) return next();
+
+    var query = url + req.query.url;
+    request(query, (err, req, body) => {
+        var $ = cheerio.load(body);
+        var links = [];
+
+        $('div.page-content div').children().each((i, elem) => {
+            links.push($(elem).attr('href'));
+        });
+
+        res.send(links);
+    });
+});
+
+app.get('*', (req, res) => {
+    res.send({error: ':('});
+});
+
+app.listen(8030);
