ffd9a7f327321d16c338a04f6e0193c32979a4f6
Logan McAnsh <logan@mcan.sh>
2017-06-01 12:55:30 -0400

    wildcard param
    makes the url
    `http://localhost:8030/https://open.spotify.com/track/4fbvXwMTXPWaFyaMWUm9CR`
    instead of using the query

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const request = require('request');
 const app = express();
 const url = 'https://songlink.io/';
 
-app.get('/', (req, res, next) => {
-  if (!req.query || !req.query.url) return next();
+app.get('/*', (req, res, next) => {
+  const queryURL = req.params[0];
+  if (!req.params || !queryURL) return next();
 
-  const query = url + req.query.url;
+  const query = url + queryURL;
   request(query, (err, req, body) => {
     const $ = cheerio.load(body);
-    var links = [];
+    let links = [];
 
     $('div.page-content div').children().each((_, elem) => {
       links.push($(elem).attr('href'));
@@ -23,7 +24,7 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('*', (req, res) => {
-  res.send({error: ':('});
+  res.send({ error: ':(' });
 });
 
 app.listen(8030);
